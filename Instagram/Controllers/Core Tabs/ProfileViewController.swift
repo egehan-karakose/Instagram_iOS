b8563f74ea732d9c633fb39cd5ddcf246c30e9ac
egehan205 <egehankarakose25@hotmail.com>
2020-12-08 12:23:05 +0300

    first commit

---
+++ Instagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Instagram
+//
+//  Created by Egehan Karaköse on 8.12.2020.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
