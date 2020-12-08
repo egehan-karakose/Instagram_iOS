e219c0ccdfabf1857c271ce0c54504356e569616
egehan205 <egehankarakose25@hotmail.com>
2020-12-08 11:58:06 +0300

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Egehan Karaköse on 8.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
